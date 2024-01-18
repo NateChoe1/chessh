644d8e05b8b9ded1b2250f8dc5023ecda82fdc8c
Nate Choe <nate@natechoe.dev>
2024-01-18 17:40:51 -0600

    Better graphics and UX

--- src/client/runner.c
+++ src/client/runner.c
@@ -25,6 +25,7 @@
 #include <client/runner.h>
 
 static int parse_cmd(struct game *game, char *move);
+static void print_letters(int player);
 static void print_board(struct game *game, int player);
 
 int run_client(int sock_fd) {
@@ -80,40 +81,46 @@ again:
 
 static int parse_cmd(struct game *game, char *move) {
 	struct move move_s;
-	move_s.r_i = move[0] - '0';
-	move_s.c_i = move[1] - '0';
-	move_s.r_f = move[2] - '0';
-	move_s.c_f = move[3] - '0';
+	move_s.c_i = move[0] - 'a';
+	move_s.r_i = 8 - (move[1] - '0');
+	move_s.c_f = move[2] - 'a';
+	move_s.r_f = 8 - (move[3] - '0');
 	return make_move(game, &move_s);
 }
 
-static void print_board(struct game *game, int player) {
-	for (int i = player ? 7:0; i != (player ? -1:8); i += player ? -1:1) {
-		for (int j = player ? 7:0; j != (player ? -1:8); j += player ? -1:1) {
-			struct piece *piece = &game->board.board[i][j];
-			char filler = (i+j)%2==0 ? ' ':'#';
-			char pc = '.';
-			switch (piece->type) {
-			case ROOK: pc = 'r'; break;
-			case KNIGHT: pc = 'n'; break;
-			case BISHOP: pc = 'b'; break;
-			case QUEEN: pc = 'q'; break;
-			case KING: pc = 'k'; break;
-			case PAWN: pc = 'p'; break;
-			case EMPTY: pc = filler; break;
-			}
-			if (piece->type != EMPTY) {
-				if (piece->player == WHITE) {
-					fputs("\033[47;30m", stdout);
-				}
-				else {
-					fputs("\033[40;37m", stdout);
-				}
-			}
-			putchar(pc);
-			fputs("\033[0m", stdout);
-			putchar(filler);
-		}
-		putchar('\n');
+static void print_letters(int player) {
+	if (player == 0) {
+		puts("  a b c d e f g h");
 	}
+	else {
+		puts("  h g f e d c b a");
+	}
+}
+
+static void print_board(struct game *game, int player) {
+	print_letters(player);
+	for (int i = 0; i < 8; ++i) {
+		int row = player == 0 ? i : 7-i;
+		printf("%d ", 8 - row);
+		for (int j = 0; j < 8; ++j) {
+			int col = player == 0 ? j : 7-j;
+			struct piece *piece = &game->board.board[row][col];
+			char *sym = "  ";
+
+			fputs((row+col)%2==0 ? "\33[45":"\33[44", stdout);
+			fputs(piece->player==0 ? ";37m":";30m", stdout);
+			switch (piece->type) {
+			case ROOK:   sym = "♜ "; break;
+			case KNIGHT: sym = "♞ "; break;
+			case BISHOP: sym = "♝ "; break;
+			case QUEEN:  sym = "♛ "; break;
+			case KING:   sym = "♚ "; break;
+			case PAWN:   sym = "♟︎ "; break;
+			case EMPTY:  sym = "  "; break;
+			}
+			fputs(sym, stdout);
+		}
+		printf("\33[0m %d\n", 8 - row);
+	}
+	print_letters(player);
 }
